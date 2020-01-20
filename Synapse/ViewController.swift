ba89da52144b7f10d066f73d157d3f8500d803f9
Allison Trager <allisontrager4@gmail.com>
2020-01-19 21:13:56 -0600

    Initial Commit

---
+++ Synapse/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Synapse
+//
+//  Created by Allison Trager on 1/19/20.
+//  Copyright © 2020 Synapse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
