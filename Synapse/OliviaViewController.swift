0a3481b3d8b3a8e39f10f05ee25b02344f6e8b8d
zlnoble <noblezack@gmail.com>
2020-01-27 22:22:47 -0600

    Landing Screen With Links

---
+++ Synapse/OliviaViewController.swift
@@ -0,0 +1,30 @@
+//
+//  OliviaViewController.swift
+//  Synapse
+//
+//  Created by Zack Noble on 1/27/20.
+//  Copyright © 2020 Synapse. All rights reserved.
+//
+
+import UIKit
+
+class OliviaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
