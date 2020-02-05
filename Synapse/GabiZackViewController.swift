be893db7f340757317c7ad1d5f208589b3952b02
zlnoble <noblezack@gmail.com>
2020-02-04 23:34:08 -0600

    some more changes

--- Synapse/GabiZackViewController.swift
+++ Synapse/GabiZackViewController.swift
@@ -8,9 +8,10 @@
 
 import UIKit
 
-class GabiZackViewController: UIViewController, UITableViewDataSource {
+class GabiZackViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
+    
     var myCoursesArray = [Course]()
     
     override func viewDidLoad() {
@@ -27,37 +28,27 @@ class GabiZackViewController: UIViewController, UITableViewDataSource {
         myCoursesArray.append(Course(course: "MGMT 3401", teacher: "Il Sung"))
     }
     
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return myCoursesArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") as? TableView else {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath) as? TableView else {
             return UITableViewCell()
         }
-        
         cell.courseLabel.text = myCoursesArray[indexPath.row].course
         cell.teacherLabel.text = myCoursesArray[indexPath.row].teacher
         
         return cell
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
 
 class Course {
-    let course: String
-    let teacher: String
+    var course: String
+    var teacher: String
     
     init(course: String, teacher: String) {
         self.course = course
