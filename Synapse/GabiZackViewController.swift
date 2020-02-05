6a442315c5e757b95ec2fa1dea970c03e1e69dea
zlnoble <noblezack@gmail.com>
2020-02-04 23:14:13 -0600

    temporary

--- Synapse/GabiZackViewController.swift
+++ Synapse/GabiZackViewController.swift
@@ -8,12 +8,38 @@
 
 import UIKit
 
-class GabiZackViewController: UIViewController {
+class GabiZackViewController: UIViewController, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    var myCoursesArray = [Course]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpCourses()
 
         // Do any additional setup after loading the view.
+    }
+    private func setUpCourses() {
+        myCoursesArray.append(Course(course: "MATH 1011", teacher: "Stalin"))
+        myCoursesArray.append(Course(course: "ARTS 2349", teacher: "Zedong"))
+        myCoursesArray.append(Course(course: "CS 3493", teacher: "Mussolini"))
+        myCoursesArray.append(Course(course: "SOC 1930", teacher: "Churchill"))
+        myCoursesArray.append(Course(course: "MGMT 3401", teacher: "Il Sung"))
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myCoursesArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") as? TableView else {
+            return UITableViewCell()
+        }
+        
+        cell.courseLabel.text = myCoursesArray[indexPath.row].course
+        cell.teacherLabel.text = myCoursesArray[indexPath.row].teacher
+        
+        return cell
     }
     
 
@@ -27,4 +53,14 @@ class GabiZackViewController: UIViewController {
     }
     */
 
+}
+
+class Course {
+    let course: String
+    let teacher: String
+    
+    init(course: String, teacher: String) {
+        self.course = course
+        self.teacher = teacher
+    }
 }
