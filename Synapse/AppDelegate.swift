6d0d4e69e36b7922473b7523428889ae7a4b74ae
Allison Trager <allisontrager4@gmail.com>
2020-02-09 15:19:47 -0600

    Edited nav bar

--- Synapse/AppDelegate.swift
+++ Synapse/AppDelegate.swift
@@ -19,6 +19,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         FirebaseApp.configure()
         UITabBar.appearance().barTintColor = .gray
         UITabBar.appearance().tintColor = .black
+        UISearchBar.appearance().tintColor = .white
+        //UINavigationBar.appearance().tintColor = UIColor(hex: "#BB86FC")
+        UINavigationBar.appearance().titleTextAttributes = [
+            NSAttributedString.Key.font : UIFont(name: "Avenir", size: 20)!,
+            NSAttributedString.Key.foregroundColor : UIColor(hexString: "3700B3")
+        ]
         return true
     }
 
@@ -39,3 +45,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 }
 
+
+extension UIColor {
+    convenience init?(hexString: String) {
+        var chars = Array(hexString.hasPrefix("#") ? hexString.dropFirst() : hexString[...])
+        switch chars.count {
+        case 3: chars = chars.flatMap { [$0, $0] }; fallthrough
+        case 6: chars = ["F","F"] + chars
+        case 8: break
+        default: return nil
+        }
+        self.init(red: .init(strtoul(String(chars[2...3]), nil, 16)) / 255,
+                green: .init(strtoul(String(chars[4...5]), nil, 16)) / 255,
+                 blue: .init(strtoul(String(chars[6...7]), nil, 16)) / 255,
+                alpha: .init(strtoul(String(chars[0...1]), nil, 16)) / 255)
+    }
+}
