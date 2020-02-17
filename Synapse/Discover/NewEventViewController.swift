3f6b2410056065f206bc788b89392beef61afe74
oliviatanzman <oliviatanzman@gmail.com>
2020-02-17 00:43:12 -0600

    added the date picker and the create button

--- Synapse/Discover/NewEventViewController.swift
+++ Synapse/Discover/NewEventViewController.swift
@@ -1,5 +1,5 @@
 //
-//  CreateEvent.swift
+//  NewEventViewController.swift
 //  Synapse
 //
 //  Created by Olivia Tanzman on 2/13/20.
@@ -9,7 +9,7 @@
 import DateTimePicker
 import UIKit
 
-class CreateEventViewController: UIViewController {
+class NewEventViewController: UIViewController {
 
     @IBOutlet weak var classNameTextField: UITextField!
 
@@ -18,25 +18,114 @@ class CreateEventViewController: UIViewController {
     @IBOutlet weak var descriptionTextField: UITextField!
     
     @IBOutlet weak var dateTextField: UITextField!
-    @IBOutlet weak var datePicker: UIDatePicker!
+    private var datePicker: UIDatePicker?
     
     @IBOutlet weak var timeTextField: UITextField!
+    private var timePicker: UIDatePicker?
     
     @IBOutlet weak var durationTextField: UITextField!
     
     @IBOutlet weak var locationTextField: UITextField!
-    private var dpShowDateVisible = false
     override func viewDidLoad() {
         super.viewDidLoad()
-    
-        // Do any additional setup after loading the view.
+        
+        //add a date to the date textfield here
+        datePicker = UIDatePicker()
+        datePicker?.datePickerMode = .date
+        datePicker?.addTarget(self, action: #selector(NewEventViewController.dateChanged(datePicker:)), for: .valueChanged)
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(NewEventViewController.viewTapped(gestureRecognizer:)))
+        
+        view.addGestureRecognizer(tapGesture)
+        
+        dateTextField.inputView = datePicker
+        
+        // add a time to the time textfield here
+        timePicker = UIDatePicker()
+        timePicker?.datePickerMode = .time
+        timePicker?.addTarget(self, action: #selector(NewEventViewController.timeChanged(timePicker:)), for: .valueChanged)
+        
+        let tapGesture1 = UITapGestureRecognizer(target: self, action: #selector(NewEventViewController.viewTapped1(gestureRecognizer:)))
+        
+        view.addGestureRecognizer(tapGesture1)
+        
+        timeTextField.inputView = timePicker
     
     }
-    private func toggleShowDateDatepicker () {
-        dpShowDateVisible = !dpShowDateVisible
+    //function when the date function is clicked on
+    @objc func viewTapped(gestureRecognizer: UITapGestureRecognizer){
+        view.endEditing(true)
+    }
+    //function for when the date is changed
+    @objc func dateChanged(datePicker: UIDatePicker) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MM/dd/yyyy"
+        
+        dateTextField.text = dateFormatter.string(from: datePicker.date)
+        view.endEditing(true)
+    }
     
-      }
-  
+    //function when the time function is clicked on
+    @objc func viewTapped1(gestureRecognizer: UITapGestureRecognizer){
+        view.endEditing(true)
+    }
+    //function for when the time is changed
+    @objc func timeChanged(timePicker: UIDatePicker) {
+        let timeFormatter = DateFormatter()
+        timeFormatter.dateFormat = "HH:MM"
+        
+        timeTextField.text = timeFormatter.string(from: timePicker.date)
+        view.endEditing(true)
+    }
+      
+        //configure the textfields
+        private func configureTextFields() {
+            classNameTextField.delegate = self
+            eventNameTextField.delegate = self
+            descriptionTextField.delegate = self
+            dateTextField.delegate = self
+            timeTextField.delegate = self
+            durationTextField.delegate = self
+            locationTextField.delegate = self
+        }
+        
+        //make sure that the user taps within the textfields
+        private func configureTapGesture(){
+            let tapGesture = UITapGestureRecognizer(target: self , action: #selector(NewEventViewController.handleTap))
+            view.addGestureRecognizer(tapGesture)
+        }
+        
+        @objc func handleTap(){
+            view.endEditing(true)
+        }
+        
+        //when they click the create button
+        @IBAction func CreateTapped(_ sender: Any) {
+            //think this is right! but need to be merged so that it can access the ClassEventsViewController so it is unclear right now but will discuss at meeting
+            
+            //let vcMyClasses = storyboard?.instantiateViewController(identifier: "ClassEventsViewController") as? ClassEventsViewController
+                                        
+            
+           // vcMyClasses?.courseName  = classNameTextField.text!
+            // vcMyClasses?.currentEventsArray = {descriptionTextField; dateTextField; timeTextField}
+                        
+            //          navigationController?.pushViewController(vcMyClasses!, animated: true)
+            
+            
+            
+        }
+        
+        
+        /*
+        // MARK: - Navigation
+
+        // In a storyboard-based application, you will often want to do a little preparation before navigation
+        override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+            // Get the new view controller using segue.destination.
+            // Pass the selected object to the new view controller.
+        }
+        */
+
     /*
     // MARK: - Navigation
 
@@ -49,3 +138,10 @@ class CreateEventViewController: UIViewController {
 
 
 }
+extension NewEventViewController: UITextFieldDelegate{
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
+
